6845dbef2fd58c691db12e8294c9bc9aab4b8670
Chris Pressland <mail@cpressland.io>
2022-03-11 12:53:56 +0000

    Update with teapot functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # healthz
 
-Literally a Go HTTP server that returns a HTTP 200 on /healthz. Nothing more. Project goal was to be as small as possible, this compiles down to ~5mb, goal achieved.
+Returns a `HTTP 200` on `/healthz` and a `HTTP 418` on all other requests.
+
+Designed for providing ultra basic healthcheck requirements and blackholing other requests while being as small a binary as possible.
--- main.go
+++ main.go
@@ -4,17 +4,25 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "healthy")
+}
+
+func teapot(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusTeapot)
 }
 
 func main() {
 	http.HandleFunc("/healthz", healthz)
 
-	err := http.ListenAndServe(":6502", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	r := mux.NewRouter()
+	r.PathPrefix("/healthz").HandlerFunc(healthz)
+	r.PathPrefix("/").HandlerFunc(teapot)
+	srv := &http.Server{Handler: r, Addr: ":6502"}
+	log.Fatal(srv.ListenAndServe())
 }
